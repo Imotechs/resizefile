24b836bf7a9b07d2541703bced9eff858b394ac8
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2024-12-06 23:02:15 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from "react";
+
 import * as pdfjsLib from "pdfjs-dist";
-import StarLoader from "./components/loaders/StarLoader";
-import SuccessAnimation from "./components/loaders/SuccessAnimation";
-import PreviewButton from "./components/buttons/PreviewButton";
-import FilePreviewModal from "./components/modals/FilePreviewModal";
-import { processPdf } from "./utils/processPDF";
 import PDFSplitter from "./components/PdfSplitter";
 import UnlockPDF from "./components/UnlockPDF";
 
